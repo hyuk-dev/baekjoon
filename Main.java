c9b66dea9c2fb43bbdb7ebf7e884744920313e66
이동혁 <hyuk.development@gmail.com>
2024-07-10 06:55:36 +0000

    10951

--- Main.java
+++ Main.java
@@ -4,14 +4,14 @@ import java.util.*;
 class Main {
     public static void main(String[] args) {
       Scanner sc = new Scanner(System.in);
-      int a,b;
-      while(true){
+      int a, b;
+
+      while (sc.hasNextInt()){
         a = sc.nextInt();
         b = sc.nextInt();
-
-        if(a==0 && b==0) break;
-
+        
         System.out.println(a+b);
       }
+      sc.close();
     }
 }
