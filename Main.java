295a838d1a2777c7f0fe232f1b090c01cbda0e84
이동혁 <hyuk.development@gmail.com>
2024-07-10 06:06:25 +0000

    2439

--- Main.java
+++ Main.java
@@ -2,15 +2,16 @@ import java.io.*;
 import java.util.*;
 
 class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
             Scanner sc = new Scanner(System.in);
             int n = sc.nextInt();
-
+            
             for (int i=0; i<n; i++){
-                for (int j=0; j<i+1; j++){
-                    System.out.print("*");
+                for (int j=n-1; j>=0; j--){
+                  if (j<=i) System.out.print("*");
+                  else System.out.print(" ");
                 }
-                System.out.println();
+              System.out.println();
             }
     }
 }
