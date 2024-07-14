b109342e0dd7f206c236453f73ba966d4c85f0bb
hyuk-dev <hyuk.development@gmail.com>
2024-07-14 17:45:08 +0900

    1546번

--- Main.java
+++ Main.java
@@ -3,33 +3,37 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args) {
       Scanner sc = new Scanner(System.in);
+
       int n = sc.nextInt();
-      int m = sc.nextInt();
-      int[] arr = new int[n];
-      int a, b;
+
+      int[] int_arr = new int[n];
+
       for(int i=0; i<n; i++){
-        arr[i] = i+1;
-      }
-      int bin;
-      for(int i=0; i<m; i++){
-        a = sc.nextInt() -1;
-        b = sc.nextInt() -1;
-        while(a<b){ // for문으로 하다가 계속 실패함
-          bin = arr[a];
-          arr[a++] = arr[b];
-          arr[b--] = bin;
 
-        }
-      }
-      for(int i=0;i<n;i++){
-        System.out.print(arr[i] + " ");
+        int_arr[i] = sc.nextInt();
+
       }
 
+      int max = 0;
+      int sum = 0;
 
+      for(int i=0; i<n; i++){
+        if(max < int_arr[i]) max = int_arr[i];
+      }
+
+      double[] double_arr = new double[n];
+
+      for(int i=0; i<n; i++){
+        double_arr[i] = ((double) int_arr[i]/max)*100;
+      }
+
+      double avr = 0;
+
+      for(int i=0; i<n; i++){
+        avr += double_arr[i];
+      }
+
+      System.out.print(avr/n);
 
     }
 }
-
-// 2 1 4 3 5
-// 3 4 1 2 5
-// -> 이 문제에서 역순이란 크기별로 역순이 아니라 기존 배치에서 역순으로.
