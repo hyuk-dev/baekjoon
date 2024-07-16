96b3084a916718db737d551e6c9d4bc4bf25fe18
이동혁 <hyuk.development@gmail.com>
2024-07-16 06:29:55 +0000

    27866

--- Main.java
+++ Main.java
@@ -5,23 +5,11 @@ class Main {
     public static void main(String[] args) {
       Scanner sc = new Scanner(System.in);
 
-      int[]arr = new int[42];
-      for(int i=0; i<42; i++){
-        arr[i] = 0;
-      }
-      int num;
-      for(int i = 0; i < 10; i++){
-        num = sc.nextInt();
-        arr[num%42] = 1;
-      }
+      String s = sc.next();
 
-      int count = 0;
+      int i = sc.nextInt();
 
-      for(int i=0; i<42; i++){
-        if(arr[i]==1) count++;
-      }
-
-      System.out.println(count);
+      System.out.print(s.charAt(i-1));
       
     }
 }
