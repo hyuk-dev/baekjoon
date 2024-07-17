fec9cdbb61458cf74bcef87acddb47706dfd431f
이동혁 <hyuk.development@gmail.com>
2024-07-17 05:29:06 +0000

    11654

--- Main.java
+++ Main.java
@@ -5,15 +5,8 @@ class Main {
     public static void main(String[] args) {
       Scanner sc = new Scanner(System.in);
 
-      int test = sc.nextInt();
-      String a;
-
-      for(int i=0;i<test;i++){
-        a = sc.next();
-        System.out.println(a.charAt(0)+""+a.charAt(a.length()-1));
-      }
-
-      
-      
+      char c = sc.next().charAt(0);
+      int num = (int)c;
+      System.out.print(num);
     }
 }
