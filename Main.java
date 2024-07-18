b5402db3dad6423cfe1addc806939a2a4e39085f
이동혁 <hyuk.development@gmail.com>
2024-07-18 06:59:51 +0000

    2908

--- Main.java
+++ Main.java
@@ -4,12 +4,19 @@ import java.util.*;
 class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    int count = 0;
-    String s;
-    while(sc.hasNext()){
-      s = sc.next();
-      count++;
-    }
-    System.out.print(count);
+    String a = sc.next();
+    String b = sc.next();
+
+    StringBuffer sba = new StringBuffer(a);
+    String ar = sba.reverse().toString();
+
+    StringBuffer sbb = new StringBuffer(b);
+    String br = sbb.reverse().toString();
+
+    int ai = Integer.parseInt(ar);
+    int bi = Integer.parseInt(br);
+
+    if (ai>bi) System.out.print(ai);
+    else System.out.print(bi);
   }
 }
