f9b385006e23d42e85610cb87c18d4622f0ff434
이동혁 <hyuk.development@gmail.com>
2024-07-18 06:40:44 +0000

    1152

--- Main.java
+++ Main.java
@@ -4,19 +4,12 @@ import java.util.*;
 class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    int t = sc.nextInt();
-    int r;
+    int count = 0;
     String s;
-    int m;
-    for(int i=0;i<t;i++){
-      m = sc.nextInt();
+    while(sc.hasNext()){
       s = sc.next();
-      for(int j = 0;j<s.length();j++){
-        for(int k = 0;k < m; k++){
-          System.out.print(s.charAt(j));
-        }
-      }
-      System.out.println("");
+      count++;
     }
-  }    
+    System.out.print(count);
+  }
 }
