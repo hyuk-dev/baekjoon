3c8d773ce55f6f86b76480161e9041df4f2e7d0b
이동혁 <hyuk.development@gmail.com>
2024-07-18 08:45:50 +0000

    11718

--- Main.java
+++ Main.java
@@ -2,24 +2,12 @@ import java.io.*;
 import java.util.*;
 
 class Main {
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    String s = sc.next();
-    int[]pn = new int[s.length()];
-    for(int i=0;i<s.length();i++){
-      if(s.charAt(i) == 'A' || s.charAt(i) == 'B' || s.charAt(i) == 'C') pn[i] = 2;
-      else if(s.charAt(i) == 'D' || s.charAt(i) == 'E' || s.charAt(i) == 'F') pn[i] = 3;
-      else if(s.charAt(i) == 'G' || s.charAt(i) == 'H' || s.charAt(i) == 'I') pn[i] = 4;
-      else if(s.charAt(i) == 'J' || s.charAt(i) == 'K' || s.charAt(i) == 'L') pn[i] = 5;
-      else if(s.charAt(i) == 'M' || s.charAt(i) == 'N' || s.charAt(i) == 'O') pn[i] = 6;
-      else if(s.charAt(i) == 'P' || s.charAt(i) == 'Q' || s.charAt(i) == 'R' || s.charAt(i) == 'S') pn[i] = 7;
-      else if(s.charAt(i) == 'T' || s.charAt(i) == 'U' || s.charAt(i) == 'V') pn[i] = 8;
-      else if(s.charAt(i) == 'W' || s.charAt(i) == 'X' || s.charAt(i) == 'Y' || s.charAt(i) == 'Z') pn[i] = 9;
+  public static void main(String[] args) throws IOException{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    String s;
+    while((s=br.readLine())!=null){
+      System.out.println(s);
     }
-    int t=0;
-    for(int i=0;i<s.length();i++){
-      t += pn[i]+1;
-    }
-    System.out.print(t);
   }
 }
