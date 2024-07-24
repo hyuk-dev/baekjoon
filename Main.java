4d2314b164b5611080ddc0d221b8c058e43b535a
hyuk-dev <hyuk.development@gmail.com>
2024-07-24 21:46:58 +0900

    10988

--- Main.java
+++ Main.java
@@ -3,37 +3,12 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args) {
       Scanner sc = new Scanner(System.in);
-
-      int n = sc.nextInt();
-
-      int[] int_arr = new int[n];
-
-      for(int i=0; i<n; i++){
-
-        int_arr[i] = sc.nextInt();
-
+      String s = sc.next();
+      int result = 1;
+      for(int i=0; i <= s.length()/2; i++){
+          if(!(s.charAt(i)==s.charAt(s.length()-i-1))) result = 0;
       }
-
-      int max = 0;
-      int sum = 0;
-
-      for(int i=0; i<n; i++){
-        if(max < int_arr[i]) max = int_arr[i];
-      }
-
-      double[] double_arr = new double[n];
-
-      for(int i=0; i<n; i++){
-        double_arr[i] = ((double) int_arr[i]/max)*100;
-      }
-
-      double avr = 0;
-
-      for(int i=0; i<n; i++){
-        avr += double_arr[i];
-      }
-
-      System.out.print(avr/n);
+        System.out.println(result);
 
     }
 }
