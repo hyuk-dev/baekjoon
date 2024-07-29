bb43cccd2efe4af9bd49ce94a5c1c0d25504ff57
hyuk-dev <hyuk.development@gmail.com>
2024-07-29 23:24:20 +0900

    2563

--- Main.java
+++ Main.java
@@ -4,18 +4,28 @@ import java.util.Arrays;
 class Main {
   public static void main(String[] args){
     Scanner sc = new Scanner(System.in);
-    char[][] arr = new char[5][15];
-    for(int i=0;i<5;i++){
-        String s = sc.next();
-        for(int j=0;j<s.length();j++){
-            arr[i][j] = s.charAt(j);
+    int n = sc.nextInt();
+    int[][] paper = new int[100][100];
+
+    for(int i=0;i<n;i++){
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        for(int j=a; j<a+10; j++){
+            for(int k=b; k<b+10; k++){
+                paper[k-1][j-1] += 1;
+            }
+        }
+
+
+    }
+    int width = 0;
+    for(int i=0;i<100;i++){
+        for(int j=0;j<100;j++){
+            if(paper[i][j] >= 1) width ++;
         }
     }
-    for(int j=0;j<15;j++){
-        for(int i=0;i<5;i++){
-            if(arr[i][j] != '\0') System.out.print(arr[i][j]+ "");
-        }
-    }
+    System.out.println(width);
 
   }
 }
