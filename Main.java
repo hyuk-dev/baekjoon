c35348b4325c90d73f9510ce30ede2781809e10b
hyuk-dev <hyuk.development@gmail.com>
2024-07-30 22:35:00 +0900

    2745 [복습필요]

--- Main.java
+++ Main.java
@@ -1,31 +1,19 @@
 import java.util.Scanner;
-import java.util.Arrays;
 
 class Main {
   public static void main(String[] args){
     Scanner sc = new Scanner(System.in);
-    int n = sc.nextInt();
-    int[][] paper = new int[100][100];
-
-    for(int i=0;i<n;i++){
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-
-        for(int j=a; j<a+10; j++){
-            for(int k=b; k<b+10; k++){
-                paper[k-1][j-1] += 1;
-            }
-        }
-
-
+    String n = sc.next();
+    int b = sc.nextInt();
+    int val = 0; // B진법 으로 나타냈을 때 영문으로 된 경우 숫자로 변경해서 저장
+    int result = 0;
+    int c = 1;
+    for(int i=n.length()-1; i>=0; i--){
+        val = Character.getNumericValue(n.charAt(i));
+        result += val*c;
+        c *= b;
     }
-    int width = 0;
-    for(int i=0;i<100;i++){
-        for(int j=0;j<100;j++){
-            if(paper[i][j] >= 1) width ++;
-        }
-    }
-    System.out.println(width);
-
+      if(result <= 1000000000) System.out.print(result);
+      else System.out.print("1000000000");
   }
 }
