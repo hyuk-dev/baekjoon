914b4821641693aaab5bbedb09f35faa00783c93
hyuk-dev <hyuk.development@gmail.com>
2024-08-08 20:58:51 +0900

    2292

--- Main.java
+++ Main.java
@@ -4,10 +4,15 @@ class Main {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int result = 0;
-        for(int i=0; i<n+1; i++){ // 2 3 5 9
-            result = (int)Math.pow(1+(int)Math.pow(2,i),2); // 2*2 3*3 5*5 9*9
+        int result = 1;
+        int a = 1;
+        int i = 1;
+        while(n > a){
+            a = a+6*i;
+            i++;
+            result++;
         }
+
         System.out.println(result);
     }
 }
