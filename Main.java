3594e6437d3b94a2b3e9a6f03c2253ec072c3e38
hyuk-dev <hyuk.development@gmail.com>
2024-08-12 22:37:48 +0900

    1978

--- Main.java
+++ Main.java
@@ -3,30 +3,21 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int n = 0;
-        while(true){
-            n = sc.nextInt();
-            if(n == -1) break;
-            int sum = 0;
-            for(int i=1; i<n; i++){
-                if(n%i == 0) sum += i;
-            }
-            if(n == sum){
-                System.out.print(n + " = ");
-                for(int i=1; i<n; i++){
-                    if(n%i == 0){
-                        if(i == 1) System.out.print(i);
-                        else System.out.print(" + " + i);
-                    }
-                }
-                System.out.println("");
-            }
-            else{
-                System.out.println(n + " is NOT perfect.");
+        int N = sc.nextInt();
+        int result = 0;
+        for(int i=0; i<N; i++){
 
+            int check = 0;
+            int num = sc.nextInt();
+            for(int j=1; j<=num; j++){
+                if(num % j ==0) check ++;
             }
+
+            if(check == 2) result ++;
 
         }
+        System.out.print(result);
+
 
     }
 }
