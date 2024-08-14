3325b1cf7ff4b9844c0e0ba731e6701046b94d3a
hyuk-dev <hyuk.development@gmail.com>
2024-08-14 19:31:56 +0900

    1085

--- Main.java
+++ Main.java
@@ -3,14 +3,20 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt(); // N을 소인수 분해 하라. 단, 1일때는 출력 안함.
-        for(int i=2; i<N+1; i++){
-            while(N%i ==0){
-                N = N/i;
-                System.out.println(i);
-            }
-        }
-        if(N!=1) System.out.println(N);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        int w = sc.nextInt();
+        int h = sc.nextInt();
+
+        int min = 0;
+
+        min = w-x;
+        if(h-y < min) min = h-y;
+        if(x < min) min = x;
+        if(y < min) min = y;
+
+        System.out.println(min);
+
 
     }
 }
