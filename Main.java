4719749776c9d5638cf3bf96f817aa9ecdb6c612
hyuk-dev <hyuk.development@gmail.com>
2024-08-18 22:23:43 +0900

    9063

--- Main.java
+++ Main.java
@@ -3,8 +3,27 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        long n = sc.nextInt();
-        System.out.println(4*n);
+        int x_max = -10000;
+        int y_max = -10000;
+        int x_min = 10000;
+        int y_min = 10000;
+
+        int N = sc.nextInt();
+        int x;
+        int y;
+        for(int i=0;i<N;i++){
+            x = sc.nextInt();
+            y = sc.nextInt();
+
+            if(x > x_max) x_max = x;
+            if(x < x_min) x_min = x;
+
+            if(y > y_max) y_max = y;
+            if(y < y_min) y_min = y;
+
+        }
+
+        System.out.println((x_max-x_min)*(y_max-y_min));
 
     }
 }
