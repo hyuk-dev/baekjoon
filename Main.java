b01fbdb900d188afc276f09231d70bbc7b56a693
hyuk-dev <hyuk.development@gmail.com>
2024-08-19 21:31:00 +0900

    10101

--- Main.java
+++ Main.java
@@ -3,27 +3,25 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int x_max = -10000;
-        int y_max = -10000;
-        int x_min = 10000;
-        int y_min = 10000;
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
 
-        int N = sc.nextInt();
-        int x;
-        int y;
-        for(int i=0;i<N;i++){
-            x = sc.nextInt();
-            y = sc.nextInt();
-
-            if(x > x_max) x_max = x;
-            if(x < x_min) x_min = x;
-
-            if(y > y_max) y_max = y;
-            if(y < y_min) y_min = y;
-
+        if(a == 60 && b == 60 && c == 60){
+            System.out.println("Equilateral");
         }
 
-        System.out.println((x_max-x_min)*(y_max-y_min));
+        else if(a+b+c == 180 && (a==b || a==c || b==c)){
+            System.out.println("Isosceles");
+        }
+
+        else if(a+b+c == 180 && (a!=b && b!=c && a!=c)){
+            System.out.println("Scalene");
+        }
+
+        else{
+            System.out.println("Error");
+        }
 
     }
 }
