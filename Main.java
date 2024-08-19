dca90172b75b424818cc0675d7104adbf2c34236
hyuk-dev <hyuk.development@gmail.com>
2024-08-19 22:00:00 +0900

    5073 복습

--- Main.java
+++ Main.java
@@ -3,24 +3,31 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int c = sc.nextInt();
+        int a=1;
+        int b=1;
+        int c=1;
+        while(true){
+            a=sc.nextInt();
+            b=sc.nextInt();
+            c=sc.nextInt();
 
-        if(a == 60 && b == 60 && c == 60){
-            System.out.println("Equilateral");
-        }
+            if(a==0 && b==0 && c==0) break;
+            if(a < b+c && b < a+c && c < a+b){
+                if(a == b && b == c) {
+                    System.out.println("Equilateral");
+                }
+                else if(a == b || b == c || a == c){
+                    System.out.println("Isosceles");
+                }
+                else if(a<b+c && b<a+c && c<a+b){
+                    System.out.println("Scalene");
+                }
+            }
 
-        else if(a+b+c == 180 && (a==b || a==c || b==c)){
-            System.out.println("Isosceles");
-        }
+            else{
+                System.out.println("Invalid");
+            }
 
-        else if(a+b+c == 180 && (a!=b && b!=c && a!=c)){
-            System.out.println("Scalene");
-        }
-
-        else{
-            System.out.println("Error");
         }
 
     }
