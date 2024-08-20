49f5ba845937c6e30e8551bd2d0a0c949bb0fbc7
hyuk-dev <hyuk.development@gmail.com>
2024-08-20 19:20:10 +0900

    24264

--- Main.java
+++ Main.java
@@ -3,25 +3,9 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int a=sc.nextInt();
-        int b=sc.nextInt();
-        int c=sc.nextInt();
+        int n = sc.nextInt();
 
-        while(true){
-            if(a < b+c && b < a+c && c < a+b) break;
-
-            if(a >= b+c){
-                a--;
-            }
-            else if(b >= a+c){
-                b--;
-            }
-            else if(c >= a+b){
-                c--;
-            }
-        }
-
-        System.out.println(a+b+c);
+        System.out.println((long)n*n + "\n2");
 
     }
 }
