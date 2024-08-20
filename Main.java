2b031c187454b6cc4bd818c2c1a53cb7b3f3e077
hyuk-dev <hyuk.development@gmail.com>
2024-08-20 22:47:50 +0900

    24313

--- Main.java
+++ Main.java
@@ -1,11 +1,27 @@
 import java.util.Scanner;
+import java.lang.Math;
 
 class Main {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        long n = sc.nextInt();
 
-        System.out.println((n* (n-1))/2 + "\n2");
+        int a1 = sc.nextInt();
+        int a0 = sc.nextInt();
+        int c = sc.nextInt();
+        int n0 = sc.nextInt();
+
+        int check = 0;
+
+        for(int i = n0; i<=100; i++){
+            if (a1 * i + a0 > c * i) {
+                check = 1;
+                break;
+            }
+        }
+
+        if(check == 1) System.out.println("0");
+        else System.out.println("1");
+
 
     }
 }
