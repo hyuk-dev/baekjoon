513e795a6699db9478e607ee6c2f2101717f26f4
hyuk-dev <hyuk.development@gmail.com>
2024-08-23 23:37:20 +0900

    2750

--- Main.java
+++ Main.java
@@ -1,34 +1,19 @@
 import java.util.Scanner;
+import java.util.Arrays;
 class Main {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-
-        int min_count = 5000;
-        int check = 0;
-
+        int[] arr = new int[N];
         for(int i=0; i<N; i++){
-            for(int j=0; j<N; j++){
-                if(j*3 + i*5 == N){
-                    check = 1;
-                    if(min_count > j+i) min_count = j+i;
-                }
-            }
+            arr[i] = sc.nextInt();
         }
 
-        if(check == 1) System.out.println(min_count);
-        else System.out.println("-1");
+        Arrays.sort(arr);
+
+        for(int i=0; i<N; i++){
+            System.out.println(arr[i]);
+        }
     }
 }
 
-/*
-18
-13
-8
-3
-0
-
-11
-6 3의 배수 될때까지 5로 빼준다.
-3 3의 배수가 되고나서 3으로 빼준다.
- */
